f46d3869575424664968857f6197d016a1683713
Miguel <miguel.blanca@outlook.com>
2017-04-30 19:35:09 +1000

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ module.exports = (bot) => {
 
   bot.hear(/toma/i, res => {
 
-    bot.http("https://app.ticketmaster.com/discovery/v2/events.json?apikey=kGKXIVGZGjg8PmcN902QGrd2nbrzwx9h&startDateTime=2017-04-16T11:53:00Z&endDateTime=2017-04-22T11:53:00Z&size=1&page=0&countryCode=AU").header('Accept', 'application/json').get()(function(err, response, body) {
+    bot.http("https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikey}&startDateTime=2017-04-16T11:53:00Z&endDateTime=2017-04-22T11:53:00Z&size=1&page=0&countryCode=AU").header('Accept', 'application/json').get()(function(err, response, body) {
       let data;
       data = JSON.parse(body)
       if (err) {
